8abcbe602cc784fdf78bbe9e11f41eda949e84d4
yevhen.kuleshov <yevhen.kuleshov@crossinx.com>
2025-11-08 14:25:37 +0100

    Lesson5

--- js/script.js
+++ js/script.js
@@ -1,3 +1,83 @@
+// function summ(name, price, count) {
+//   return function () {
+//     console.log(`Tovar ${name} stoit : ${price * count}`);
+//   };
+// }
+
+// document.querySelector("#b").addEventListener("click", function () {
+//   summ(
+//     document.querySelector("#t").value,
+//     Number(document.querySelector("#p").value),
+//     Number(document.querySelector("#c").value)
+//   )();
+// });
+
+// const el = {
+//   name: "",
+//   price: 0,
+//   count: 0,
+//   summ: function schowSumm() {
+//     console.log(`Tovar ${this.name} stoit : ${this.price * this.count}`);
+//   },
+// };
+
+// const b = {
+//   name: "Banana",
+//   price: 30,
+//   count: 4,
+// };
+
+// el.summ.call(b);
+
+// function info(text) {
+//   return function () {
+//     console.log(text);
+//   };
+// }
+// document
+//   .querySelector("#html")
+//   .addEventListener("click", info("text about HTML"));
+
+// document
+//   .querySelector(".css")
+//   .addEventListener("click", info("big text about CSS"));
+
+// const student = {
+//   name: "Ivan",
+//   spec: "Plamber",
+//   bal: "4",
+//   prop: "6",
+//   info: function info() {
+//     console.group(`Name ${this.name}`);
+//     console.log(`spec ${this.spec}`);
+//     console.log(`bal ${this.bal}`);
+//     console.log(`prop ${this.prop}`);
+//   },
+// };
+
+// const student1 = {
+//   name: "Ivan1",
+//   spec: "Plamber1",
+//   bal: "14",
+//   prop: "16",
+// };
+// const student2 = {
+//   name: "Ivan2",
+//   spec: "Plamber2",
+//   bal: "24",
+//   prop: "26",
+// };
+// const student3 = {
+//   name: "Ivan3",
+//   spec: "Plamber3",
+//   bal: "34",
+//   prop: "36",
+// };
+
+// student.info();
+// student.info.bind(student1)();
+// student.info.apply(student3);
+
 // let ask = (question, yes, no) => {
 //   if (confirm(question)) yes();
 //   else no();
